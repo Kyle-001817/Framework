d7e8ec6d1dfb67c38a82386413aa242a31b126bc
Kyle-001817 <104860159+Kyle-001817@users.noreply.github.com>
2023-04-28 13:53:38 +0200

    commit

---
+++ Sprint5/Test/WEB-INF/classes/Dept.java
@@ -0,0 +1,25 @@
+package model;
+
+import etu1817.framework.*;
+
+public class Dept {
+    String nom;
+    String numero;
+
+    @MethodAnnotation(url = "dept-all")
+    public String getNom() {
+        return this.nom;
+    }
+
+    @MethodAnnotation(url = "dept-one")
+    public String getNumero() {
+        return this.numero;
+    }
+
+    @MethodAnnotation(url = "test")
+    public ModelView Test() {
+        ModelView m = new ModelView();
+        m.setView("Test.jsp");
+        return m;
+    }
+}
